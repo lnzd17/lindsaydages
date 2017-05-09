78a6494dc8fde75f60e6568c293ce2cdbf698e02
lnzd17 <lindsaykayedutton@gmail.com>
2017-05-09 15:23:13 -0400

    added controllers and updated routes

---
+++ app/controllers/admin/solutions_controller.rb
@@ -0,0 +1,2 @@
+class Admin::SolutionsController < AdminController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  namespace :admin do
+    resources :solutions, only: [:new, :create, :edit, :destroy]
+    resources :projects, only: [:new, :create, :edit, :destroy]
+    resources :tools, only: [:new, :create, :edit, :destroy]
+  end
+  resources :solutions, only: [:index, :show]
+  resources :solutions, only: [:index, :show]
+  resources :solutions, only: [:index, :show]
 end
