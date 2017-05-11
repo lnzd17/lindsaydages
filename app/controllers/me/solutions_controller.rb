dc53feec704f3b6dc3860fe5d57f613762e1fa50
lnzd17 <lindsaykayedutton@gmail.com>
2017-05-11 14:17:02 -0400

    changes to views

--- app/controllers/me/solutions_controller.rb
+++ app/controllers/me/solutions_controller.rb
@@ -1,6 +1,18 @@
 class Me::SolutionsController < ApplicationController
   before_action :require_admin
-  def create
+
+  def new
     @solution = Solution.new
+  end
+
+  def create
+    @solution = Solution.create(solution_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def solution_params
+    params.require(:solution).permit(:name, :description, :snippet)
   end
 end
--- app/controllers/me/tools_controller.rb
+++ app/controllers/me/tools_controller.rb
@@ -1,6 +1,18 @@
 class Me::ToolsController < ApplicationController
   before_action :require_admin
-  def create
+
+  def new
     @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.create(tool_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :description, :logo, :level)
   end
 end
