a4ddf2dff3d88193e7d6ddfdaa1749374ac275ef
Lindsay Dages <lindsaykayedutton@gmail.com>
2019-04-14 14:38:26 -0400

    refactor js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 console.log("script loaded");
 
-window.onload = function() {
+window.onload = () => {
+  handleArrowClick();
+  displayCopyRight();
+
+};
+
+
+const handleArrowClick = () => {
   const arrow = document.getElementById("arrowContainer");
-  arrow.onclick = function() {
+  arrow.onclick = () => {
     const mySummary = document.getElementById("mySummary");
     window.scrollTo({
       top: mySummary.offsetTop - 100,
@@ -10,8 +17,10 @@ window.onload = function() {
       behavior: "smooth"
     });
   };
+}
 
+const displayCopyRight = () => {
   const copyright = document.getElementById("copyright");
   const year = new Date().getFullYear();
   copyright.innerHTML = `Copyright ${year} - Lindsay Dages`;
-};
+}
